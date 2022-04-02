36332ecef46fe822835e65f39e4104bca03ed695
AJ07-Ajay <67946416+AJ07-Ajay@users.noreply.github.com>
2022-04-02 09:05:07 -0700

    Update battleship.js
    Added Miss and Hit Alert !!

--- Writing Real Code: Going Further/battleship.js
+++ Writing Real Code: Going Further/battleship.js
@@ -6,17 +6,21 @@ var location1 = 3,
   isSunk = false;
 while (isSunk == false) {
   guess = prompt("ready, aim,fire!(Enter a number b/w 0-6)");
-  if (guess < 0 && guess > 6) {
-    alert("Enter a valid cell number ");
-    ''
+  if (guess < 0 || guess > 6)
+  {
+    alert("Enter a valid cell number");
   } else {
     guesses = guesses + 1;
     if (guess == location1 || guess == location2 || guess == location3) {
       hits += 1;
+      alert("HIT!");
       if (hits == 3) {
         isSunk = true;
         alert("You Sank my battleship!!");
       }
+    }
+    else{
+    alert("MISS")
     }
   }
 }
