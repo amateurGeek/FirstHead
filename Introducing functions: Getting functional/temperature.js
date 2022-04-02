210314425e9076c4defd3224ff0e2629ccc49266
AJ07-Ajay <67946416+AJ07-Ajay@users.noreply.github.com>
2022-04-02 10:57:18 -0700

    Create Temparatur.js
    Deciding Dress Code.

---
+++ Introducing functions: Getting functional/temperature.js
@@ -0,0 +1,13 @@
+function whatShallIWear(temp) {
+  if (temp < 60) {
+    alert("Wear a jacket");
+  } else if (temp < 70) {
+    alert("Wear a sweater");
+  } else {
+    alert("Wear a t-shirt");
+  }
+}
+
+whatShallIWear(60);
+whatShallIWear(80);
+whatShallIWear(70);
