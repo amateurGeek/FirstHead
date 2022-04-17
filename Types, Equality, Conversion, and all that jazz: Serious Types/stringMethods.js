c6e9ae774db9a1ce6716a5f5d9af19b7ebe097de
Ajay Seth <67946416+amateurGeek@users.noreply.github.com>
2022-04-17 15:32:17 +0530

    added & update code

--- Types, Equality, Conversion, and all that jazz: Serious Types/stringMethods.js
+++ Types, Equality, Conversion, and all that jazz: Serious Types/stringMethods.js
@@ -1,1 +1,13 @@
+var test="AJAY|SETH|IS|A|TESTER ajay ajay ajay ajay";
 
+var presentableText = test.toLowerCase();
+if(presentableText.length>0)
+{
+console.log(presentableText);
+}
+
+console.log(test.substring(0,4))
+console.log(test.indexOf("SETH", 4))
+console.log(test.lastIndexOf("ajay"))
+
+console.log("Hello Splitted data", test.split("|"));
