94f3c2a4b2495a9d75ec9a90f47eb7bbb4a96e00
Ajay Seth <67946416+amateurGeek@users.noreply.github.com>
2022-04-17 13:10:42 +0530

    create typeOf.js

---
+++ Types, Equality, Conversion, and all that jazz: Serious Types/typeOf.js
@@ -0,0 +1,22 @@
+
+var t1 = "ABC"
+var t2 = 123
+var t3 = true
+var t4 = {};
+var t5 = []
+var t6;
+var t7= {"abc": 123}; 
+var t8= ["Abc", 123];
+function t9 (){
+return "abc"
+}
+
+console.log(typeof t1)
+console.log(typeof t2)
+console.log(typeof t3)
+console.log(typeof t4)
+console.log(typeof t5)
+console.log(typeof t6)
+console.log(typeof t7)
+console.log(typeof t8)
+console.log(typeof t9)
