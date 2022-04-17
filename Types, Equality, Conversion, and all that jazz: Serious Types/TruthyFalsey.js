6ff139110e167c3334de2df7210e02b3efc028ea
Ajay Seth <67946416+amateurGeek@users.noreply.github.com>
2022-04-17 15:07:21 +0530

    added code

--- Types, Equality, Conversion, and all that jazz: Serious Types/TruthyFalsey.js
+++ Types, Equality, Conversion, and all that jazz: Serious Types/TruthyFalsey.js
@@ -1,1 +1,46 @@
 
+function lieDetectorTest(){
+var lies =0;
+
+var stolenDiamond={};
+if(stolenDiamond){
+console.log("You Stole the diamond");
+lies++;
+}
+var car= {
+keysInPocket: null
+};
+if (car.keysInPocket){
+console.log("Uh oh, guess you stole the car");
+lies++
+}
+if (car.emptyGasTank)
+{
+console.log("You drove the car after you stole it");
+lies++
+}
+var foundYouAtTheCrimeScene = [];
+if(foundYouAtTheCrimeScene)
+{
+console.log("A Sure sign of Guilt");
+lies++;
+}
+if(foundYouAtTheCrimeScene[0]){
+console.log("Caught with the Stole ITEM !");
+lies++;
+}
+var yourName = " ";
+if(yourName)
+{
+console.log("Gues you lied about your name");
+lies++;
+}
+return lies;
+}
+
+var numberOfLies = lieDetectorTest();
+console.log(" You told " + numberOfLies+ " Lies !");
+
+if (numberOfLies >=3){
+console.log("Charge of Guilt");
+}
